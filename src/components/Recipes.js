24c84888128b9cb5e49b59f606aab27850bbb5d7
unknown <hannahhtn97@gmail.com>
2022-08-12 09:15:00 -0700

    Added recipe's title, picture and direction. Mobile design finished

--- src/components/Recipes.js
+++ src/components/Recipes.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
 import RecipeBtn from "./RecipeBtn";
-import './Recipes.css';
+import "./Recipes.css";
 
 class Recipes extends Component {
 	state = {
 		recipeTitle: "",
 		recipeImg: "",
+		recipeInstruction: [],
 	};
 
 	componentDidMount() {
@@ -14,40 +15,51 @@ class Recipes extends Component {
 				return res.json();
 			})
 			.then((data) => {
-				// this.setState({ recipeTitle: data.recipes[0].title });
-				// this.setState({ recipeImg: data.recipes[0].image });
+				console.log(data);
 				this.setState({ recipeTitle: data.meals[0].strMeal });
 				this.setState({ recipeImg: data.meals[0].strMealThumb });
+				this.setState({ recipeID: data.meals[0].idMeal });
+                const array = data.meals[0].strInstructions.split(/\r?\n/);
+				this.setState({
+					recipeInstruction: array
+				});
+                console.log(array);
 			});
 	}
 
 	handleRecipeRequest = async () => {
-		// https://www.themealdb.com/api/json/v1/1/random.php
-		// fetch(
-		// 	`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}`
-		// )
 		fetch("https://www.themealdb.com/api/json/v1/1/random.php")
 			.then((res) => {
 				return res.json();
 			})
 			.then((data) => {
-				// this.setState({ recipeTitle: data.recipes[0].title });
-				// this.setState({ recipeImg: data.recipes[0].image });
-                this.setState({ recipeTitle: data.meals[0].strMeal });
+				this.setState({ recipeTitle: data.meals[0].strMeal });
 				this.setState({ recipeImg: data.meals[0].strMealThumb });
+				this.setState({ recipeID: data.meals[0].idMeal });
+				const array = data.meals[0].strInstructions.split(/\r?\n/);
+				this.setState({
+					recipeInstruction: array,
+				});
 			});
 	};
 
 	render() {
 		return (
-			<div>
-				<h1>{this.state.recipeTitle}</h1>
-				<img
-					src={this.state.recipeImg}
-					className="img-thumbnail"
-					alt={this.state.recipeTitle}
-				></img>
-				<RecipeBtn onRecipeRequest={this.handleRecipeRequest} />
+			<div className="container">
+				<div>
+					<h1>{this.state.recipeTitle}</h1>
+					<img
+						src={this.state.recipeImg}
+						className="img-thumbnail"
+						alt={this.state.recipeTitle}
+					></img>
+					<RecipeBtn onRecipeRequest={this.handleRecipeRequest} />
+				</div>
+				<div>
+					{this.state.recipeInstruction.map((item) => {
+						return <p key={this.state.recipeInstruction.indexOf(item)}>{item}</p>;
+					})}
+				</div>
 			</div>
 		);
 	}
